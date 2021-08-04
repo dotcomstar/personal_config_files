d3a4ec84cf7f29b73d5056fc2da84a6c5f676120
Jet Lee <gamestar109@live.com>
2021-08-04 01:24:46 -0400

    Create update_binds.bat

---
+++ CSGO/update_binds.bat
@@ -0,0 +1,5 @@
+:: Suppress verbose output of each line in the file (prints all by default).
+@echo off
+"[put path to python executable here]" "[put path to generate_binds.py here]"
+START steam://rungameid/730
+::pause
