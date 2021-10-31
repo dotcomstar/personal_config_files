77870e08fa0c74274dac940dde5e9180ad5621d5
Jet Lee <gamestar109@live.com>
2021-10-31 15:48:24 -0400

    Add support for arbitrary multi-line copypastas

--- CSGO/generate_binds.py
+++ CSGO/generate_binds.py
@@ -1,10 +1,11 @@
 # This script will generate looping CSGO binds for each non-blank line in the given file.
 
 import random
-import os 
+import os
 from os import listdir
 
-copypasta_path = "[insert path here]"
+#copypasta_path = "/d/Downloads/Video Games/Steam/userdata/333549295/730/local/cfg/dwayne/"  # Linux
+copypasta_path = "D:\\Downloads\\Video Games\\Steam\\userdata\\333549295\\730\\local\cfg\\dwayne\\"  # Windows
 copypastas_file = "copypasta.txt"
 keybind = "F9"
 
@@ -13,21 +14,24 @@ for file_name in listdir(copypasta_path):
     if file_name.endswith('.cfg'):
         os.remove(copypasta_path + file_name)
 
-lines = open(copypastas_file).read().split('\n\n')
+lines = open(copypastas_file, encoding='utf-8').read().split('\n\n')
 random.shuffle(lines)
 counter = 1
-for line in lines:
-    if line == '':
-        lines.remove(line)
-for line in lines:
-    file_path = str(counter) + ".cfg" 
-    fp = open(file_path, 'w+')
-    fp.write('say ' + line + ';' + ' bind "' + keybind + '" "exec dwayne/' + str(counter + 1) + '"')
-    text = line
-    counter += 1
+for quote in lines:
+    if quote == '':
+        continue
+    quote = quote.split("\n")
+    for line in quote:
+        if line == '':
+            continue
+        file_name = str(counter) + ".cfg"
+        fn = open(file_name, 'w+')
+        fn.write('say ' + line + ';' + ' bind "' + keybind + '" "exec dwayne/' + str(counter + 1) + '"')
+        text = line
+        counter += 1
 
 # Make the binds loop back around.
-file_path = str(len(lines)) + ".cfg" 
-fp = open(file_path, 'w+')
-fp.write('say ' + text + ';' + ' bind "' + keybind + '" "exec dwayne/' + str(1) + '" ')
-# TODO: Figure out why this last line doesn't work without the space after the last quote
+file_name = str(counter - 1) + ".cfg"
+padding = ' ' * (counter // 10)  # We need this because the last line gets overwritten, not deleted and rewritten. Otherwise, we could be left with extra numbers or quotation marks.
+fn = open(file_name, 'w+')
+fn.write('say ' + text + ';' + ' bind "' + keybind + '" "exec dwayne/' + str(1) + '"' + padding)
